051d2765df34771486eeaeaa4dbb6c48d9f96b76
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-04-06 16:26:44 +0530

    Time: 71 ms (22.70%), Space: 30.1 MB (14.82%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int f(int i,int j,string s,string t,vector<vector<int>>dp)
-    {
-        if(j<0)
-            return 1;
-        if(i<0)
-            return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+//     int f(int i,int j,string s,string t,vector<vector<int>>dp)
+//     {
+//         if(j<0)
+//             return 1;
+//         if(i<0)
+//             return 0;
+//         if(dp[i][j]!=-1)return dp[i][j];
         
-        if(s[i]==t[j])
-            return dp[i][j]=f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
-        else
-            return dp[i][j]=f(i-1,j,s,t,dp);
-    }
+//         if(s[i]==t[j])
+//             return dp[i][j]=f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
+//         else
+//             return dp[i][j]=f(i-1,j,s,t,dp);
+//     }
     
     int numDistinct(string s, string t) {
         vector<vector<unsigned long long >> dp(s.length()+1,vector<unsigned long long>(t.length()+1,-1));
