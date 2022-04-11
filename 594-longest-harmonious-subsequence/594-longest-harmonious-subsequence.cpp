676fb7dfea64f8a583a303b2bca9b93dd0453323
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-04-11 23:13:33 +0530

    Time: 71 ms (95.50%), Space: 32.5 MB (91.61%) - LeetHub

--- 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution()
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+        std::cout.tie(NULL);
+    }
     int findLHS(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int i=0,j=1,ans=0,len=0;
