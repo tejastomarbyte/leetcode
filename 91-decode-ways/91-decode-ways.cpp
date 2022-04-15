31286c40577d9877ad0c104a96ae27ac608cf068
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-04-15 16:24:53 +0530

    Time: 4 ms (39.04%), Space: 7.5 MB (17.41%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int f(string s,int i,vector<int> &dp)
+    {
+        if(i==s.length()-1)
+        {
+            if(s[i]=='0')
+                return 0;
+            else
+                return 1;
+        }
+        if(i>=s.length())
+            return 1;
+        if(s[i]=='0')return 0;
+        if(dp[i]!=-1)return dp[i];
+        
+        int l=f(s,i+1,dp);
+        
+        string t;
+        t+=s[i];
+        t+=s[i+1];
+        int r=0;
+        if(t<="26")
+        {
+            r=f(s,i+2,dp);
+        }
+        return dp[i]=l+r;
+    }
+    int numDecodings(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        vector<int>dp(s.length(),-1);
+        int ans=f(s,0,dp);
+        return ans;
+    }
+};
