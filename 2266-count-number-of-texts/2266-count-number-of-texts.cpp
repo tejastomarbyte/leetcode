cb22546ea4c3960a5499e253a2d87425c5a29c5c
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-05-19 00:11:06 +0530

    Time: 119 ms (26.21%), Space: 15.2 MB (70.44%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.cpp
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -1,30 +1,6 @@
 const int mod=1e9+7;
 class Solution {
 public:
-    // int f(string p,int i,int n,vector<int>&dp)
-    // {
-    //     if(i>=n)return 1;
-    //     if(dp[i]!=-1)return dp[i];
-    //     int ans=0;
-    //     if(i+1<n && p[i]==p[i+1])
-    //     {
-    //         ans+=f(p,i+2,n,dp);
-    //         ans=(ans)%mod;
-    //         if(i+2<n && p[i]==p[i+2])
-    //         {
-    //             ans+=f(p,i+3,n,dp);
-    //             ans=(ans)%mod;
-    //             if(i+3<n && p[i]==p[i+3] && (p[i]=='7' || p[i]=='9'))
-    //             {
-    //                 ans+=f(p,i+4,n,dp);
-    //                 ans=(ans)%mod;
-    //             }
-    //         }
-    //     }
-    //     ans+=f(p,i+1,n,dp);
-    //     ans=ans%mod;
-    //     return dp[i]=ans;
-    // }
     int countTexts(string p) {
         int n=p.length();
         vector<int>dp(p.length()+1,0);
