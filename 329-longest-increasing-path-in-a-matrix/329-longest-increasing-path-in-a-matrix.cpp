5d20911eeea59e6fcf2ce03e892510f6bd727c31
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-05-19 13:49:22 +0530

    Time: 255 ms (12.10%), Space: 33.9 MB (13.85%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&m,int i,int j,vector<vector<bool>>&vis,vector<vector<int>>&dp)
+    int dp[201][201];
+    vector<int>dx={1,-1,0,0},dy={0,0,-1,1};
+    int r,c;
+    bool isvalid(int i,int j)
+    {
+        if(i>=0 && j>=0 && i<r && j<c)return true;
+        return false;
+    }
+    int dfs(vector<vector<int>>&m,int i,int j)
     {
         if(i<0 || i>=m.size() || j<0 || j>m[0].size())
             return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        vis[i][j]=true;
         
-        int left=0,right=0,up=0,down=0;
-        
-        if(j-1>=0 && m[i][j-1]>m[i][j])
-            left=1+dfs(m,i,j-1,vis,dp);
-        if(j+1<m[0].size() && m[i][j+1]>m[i][j])
-            right=1+dfs(m,i,j+1,vis,dp);
-        if(i-1>=0 && m[i-1][j]>m[i][j])
-            up=1+dfs(m,i-1,j,vis,dp);
-        if(i+1<m.size() && m[i+1][j]>m[i][j])
-            down=1+dfs(m,i+1,j,vis,dp);
-        
-        return dp[i][j]=max(max(left,right),max(up,down));
+        int fin=0;
+        for(int p=0;p<4;p++)
+        {
+            int ans=0;
+            if(isvalid(i+dx[p],j+dy[p]) && m[i+dx[p]][j+dy[p]]>m[i][j])
+                ans=1+dfs(m,i+dx[p],j+dy[p]);
+            fin=max(fin,ans);
+        }
+        return dp[i][j]=fin;
          
     }
     int longestIncreasingPath(vector<vector<int>>& m) {
         int longest=0;
-        vector<vector<int>>dp(m.size(),vector<int>(m[0].size(),-1));
+        r=m.size();
+        c=m[0].size();
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<m.size();i++)
         {   vector<vector<bool>>vis(m.size(),vector<bool>(m[0].size(),false));
-
             for(int j=0;j<m[i].size();j++)
             {
-                int ans=1+dfs(m,i,j,vis,dp);
+                int ans=1+dfs(m,i,j);
                 longest=max(longest,ans);
             }
         }
