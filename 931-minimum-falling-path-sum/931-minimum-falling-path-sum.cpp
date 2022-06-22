e8b9e94b747b7692d885febd8176fe4b18d66fb7
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-06-23 00:24:28 +0530

    Time: 29 ms (20.59%), Space: 10.2 MB (47.45%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
-    int f(vector<vector<int>>&ma,int i,int j,vector<vector<int>>&dp)
-    {
+//     int f(vector<vector<int>>&ma,int i,int j,vector<vector<int>>&dp)
+//     {
         
-        if(i==ma.size()-1)
-            return ma[i][j];
+//         if(i==ma.size()-1)
+//             return ma[i][j];
         
-        if(dp[i][j]!=-1)return dp[i][j];
+//         if(dp[i][j]!=-1)return dp[i][j];
         
-        int left=INT_MAX,right=INT_MAX,down;
-        if(j>=1)
-            left=f(ma,i+1,j-1,dp);
-            down=f(ma,i+1,j,dp);
-        if(j<ma[0].size()-1)
-            right=f(ma,i+1,j+1,dp);
+//         int left=INT_MAX,right=INT_MAX,down;
+//         if(j>=1)
+//             left=f(ma,i+1,j-1,dp);
+//             down=f(ma,i+1,j,dp);
+//         if(j<ma[0].size()-1)
+//             right=f(ma,i+1,j+1,dp);
         
-        return dp[i][j]=ma[i][j]+min({left,right,down});
+//         return dp[i][j]=ma[i][j]+min({left,right,down});
         
-    }
+//     }
     int minFallingPathSum(vector<vector<int>>& ma) {
-        vector<vector<int>>dp(ma.size()+1,vector<int>(ma[0].size()+1,-1));
+        vector<vector<int>>dp(ma.size(),vector<int>(ma[0].size(),-1));
         
         int ans=INT_MAX;
         
-        for(int i=0;i<ma[0].size();i++)
+        for(int i=0;i<ma.size();i++)
         {
-            ans=min(ans,f(ma,0,i,dp));
+            for(int j=0;j<ma[0].size();j++)
+            {
+                if(i==0)
+                    dp[i][j]=ma[i][j];
+                
+                else
+                {
+                    int left=INT_MAX,right=INT_MAX,down;
+                    if(j>=1)
+                        left=dp[i-1][j-1];
+                    down=dp[i-1][j];
+                    if(j<ma[0].size()-1)
+                        right=dp[i-1][j+1];
+                    dp[i][j]=ma[i][j]+min({left,right,down});
+                }
+            }
         }
-        return ans;
+        int n=ma.size();
+        return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
 };
