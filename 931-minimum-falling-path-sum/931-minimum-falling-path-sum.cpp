cc2b0ed1761e1c3d19fb52d9be7aab2387abe9c2
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-06-23 00:24:43 +0530

    Time: 30 ms (18.59%), Space: 10.3 MB (23.83%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-//     int f(vector<vector<int>>&ma,int i,int j,vector<vector<int>>&dp)
-//     {
-        
-//         if(i==ma.size()-1)
-//             return ma[i][j];
-        
-//         if(dp[i][j]!=-1)return dp[i][j];
-        
-//         int left=INT_MAX,right=INT_MAX,down;
-//         if(j>=1)
-//             left=f(ma,i+1,j-1,dp);
-//             down=f(ma,i+1,j,dp);
-//         if(j<ma[0].size()-1)
-//             right=f(ma,i+1,j+1,dp);
-        
-//         return dp[i][j]=ma[i][j]+min({left,right,down});
-        
-//     }
     int minFallingPathSum(vector<vector<int>>& ma) {
         vector<vector<int>>dp(ma.size(),vector<int>(ma[0].size(),-1));
         
