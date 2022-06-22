58ed2bd1030287ee35760587e6ac729982d906f1
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-06-22 22:28:13 +0530

    Time: 32 ms (51.25%), Space: 18.3 MB (12.95%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int n=r.size();
+        vector<int>left(n);
+        vector<int>right(n);
+        left[0]=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(r[i]>r[i-1])
+                left[i]=left[i-1]+1;
+            else
+                left[i]=1;
+        }
+        right[n-1]=1;
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            if(r[i]>r[i+1])
+                right[i]=right[i+1]+1;
+            else
+                right[i]=1;
+        }
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+            ans+=max(left[i],right[i]);
+        return ans;
+    }
+};
