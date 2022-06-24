97c2ee34ea8b3d1d5eff2f233db08abcba8e1287
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-06-24 20:30:20 +0530

    Added solution - LeetHub

---
+++ BBT counter - GFG/bbt-counter.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+typedef long long int ll;
+const int mod=1e9+7;
+class Solution {
+  public:
+    ll f(ll n,vector<ll>&dp)
+    {
+        if(n==0 || n==1)
+         return 1;
+         if(dp[n]!=-1)return dp[n];
+         
+        return dp[n]=(f(n-1,dp) * (f(n-1,dp)+2*f(n-2,dp))) % mod;
+    }
+    long long int countBT(int h) { 
+        // code here
+        vector<ll>dp(h+1,-1);
+        
+        return f(h,dp);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int h;
+        cin >> h;
+        Solution ob;
+        cout<<ob.countBT(h)<<endl;  
+    }
+    return 0;
+}
+  // } Driver Code Ends
