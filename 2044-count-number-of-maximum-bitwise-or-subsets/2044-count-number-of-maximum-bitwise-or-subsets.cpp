fc0cac62eabe07c5e0c2dd06eb0619a7f2a03ca1
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-07-04 01:01:03 +0530

    Time: 38 ms (63.76%), Space: 8.4 MB (51.45%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int count;
+    void f(vector<int>&nums,int i,int ans,int temp)
+    {
+        if(i==nums.size())
+        {
+            if(ans==temp)
+               count++;
+            return;
+        }
+        if(temp>ans)
+            return;
+        f(nums,i+1,ans,temp|nums[i]);
+        
+        f(nums,i+1,ans,temp);
+        
+        
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        int ans=0;
+        count=0;
+        for(int i=0;i<32;i++)
+        {
+            bool flag=false;
+            
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[j]&(1<<i))
+                {
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag)
+                ans+=(1<<i);
+        }
+        f(nums,0,ans,0);
+        return count;
+    }
+};
