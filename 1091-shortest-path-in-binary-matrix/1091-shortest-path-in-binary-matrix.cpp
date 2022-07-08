f1a3cabbc4541ea1bf9c29949cb60021824547c7
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-07-08 20:31:15 +0530

    Time: 152 ms (30.04%), Space: 19.7 MB (59.10%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,95 +1,39 @@
 class Solution {
 public:
-//     bool isvalid(vector<vector<int>>g,int i,int j,vector<vector<bool>>&vis)
-//     {
-//         if(i>=0 && i<g.size() && j>=0 && j<g[0].size() && !vis[i][j] && g[i][j]==0)
-//             return true;
-//         return false;
-//     }
-//     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-//         // int ans=1;
-//         vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
-//         if(grid[0][0]==1)
-//             return -1;
-        
-//         queue<pair<int,int>>q;
-//         q.push({0,0});
-//         vis[0][0]=true;
-//         int n=grid.size(),m=grid[0].size();
-//         int ans=0;
-//         while(!q.empty())
-//         {
-//             int size=q.size();
-//             ans++;
-//             for(int tt=0;tt<size;tt++)
-//             {
-//                 pair<int,int>p=q.front();
-//                 q.pop();
-//                 int i=p.first;
-//                 int j=p.second;
-                
-//                 if(i==n-1 && j==m-1)
-//                     return ans;
-                 
-//                  for(int p=0;p<8;p++)
-//                  {
-//                       int ii=i+dir[p].first;
-//                       int jj=j+dir[p].second;
-//                       if(isvalid(grid,ii,jj,vis))
-//                         {
-//                             q.push({ii,jj});
-//                             vis[ii][jj]=true;
-//                         }
-//                  }
-                        
-                    
-                
-//             }
-            
-//         }
-//         return -1;
-            
-//     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
-        int n= grid.size();
-        vector<vector<int>>vis(n, vector<int>(n,0));
-        
-        if(grid[0][0]==1)return -1;
-        
         queue<pair<int,int>>q;
-        int step=1,ans=1e9;
-        int dr[8][2]={{-1,-1},{-1,1},{1,-1},{1,1},{0,1},{0,-1},{1,0},{-1,0}};
+        int n=grid.size();
+        vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
+        if(grid[0][0]==1 or grid[n-1][n-1]==1)
+            return -1;
+        vis[0][0]=true;
         q.push({0,0});
-        while(!q.empty()){
-            int size= q.size();
-            for(int i=0 ; i<size ; i++){
-                int curi=q.front().first;
-                int curj=q.front().second;
-                if(curi==curj&& curi==n-1){
-                    ans=min(ans,step);
-                }
+        int dist=1;
+        vector<pair<int,int>> d={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size--)
+            {
+                int a=q.front().first,b=q.front().second;
                 q.pop();
-                for(int k=0 ; k<8 ; k++){
-                    
-                    int x=curi+dr[k][0];
-                    int y=curj+dr[k][1];
-                    if(x>=0 && y>=0 && x<n && y<n && grid[x][y]==0&& !vis[x][y]){
-                        
-                        vis[x][y]=1;
-                        q.push({x,y});
-                        
-                    }
-                    
-                }
                 
+                if(a==grid.size()-1 && b==grid[0].size()-1)
+                    return dist;
+                
+                for(int i=0;i<8;i++)
+                {
+                    int x1=a+d[i].first,y1=b+d[i].second;
+                    if(x1>=0 && y1>=0 && x1<grid.size() && y1<grid.size() && vis[x1][y1]==false && grid[x1][y1]==0)
+                    {
+                        vis[x1][y1]=true;
+                        q.push({x1,y1});
+                    }
+                }
             }
+            dist++;
             
-            step++;
         }
-        
-        if(ans==1e9)return -1;
-        return ans;
-        
+        return -1;
     }
 };
