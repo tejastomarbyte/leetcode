9a15c1176e27013ced8ee02778d04f70291769e7
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-07-08 12:38:59 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -9,55 +9,37 @@ using namespace std;
 
 class Solution{
 public:
-   //recursive+memoize
-    // int f(int r,int c,int n,vector<vector<int>> m,vector<vector<int>> &dp)
-    // {
-    //     if(r<0 || r>n || c<0 || c>n)
-    //     return 0;
-    //     if(r==n)
-    //     {
-    //         return m[n][c];
-    //     }
-    //     if(dp[r][c]!=-1)return dp[r][c];
-    //     int left=m[r][c]+f(r+1,c,n,m,dp);
-    //     int diag=m[r][c]+f(r+1,c-1,n,m,dp);
-    //     int down=m[r][c]+f(r+1,c+1,n,m,dp);
-        
-    //     return dp[r][c]=max(left,max(diag,down));
-        
-    // }
-    int maximumPath(int n, vector<vector<int>> m)
+    int maximumPath(int n, vector<vector<int>> ma)
     {
         // code here
-        int ans=INT_MIN;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        for(int i=0;i<n;i++)
-        dp[n-1][i]=m[n-1][i];
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=n-1;j>=0;j--)
-            {   
-                
-                int left=INT_MIN;
-                int diag=INT_MIN;
-                int down=INT_MIN;
-                
-                if(i+1<=n-1)
-                left=m[i][j]+dp[i+1][j];
-                if(i+1<=n-1 && j+1<=n-1)
-                diag=m[i][j]+dp[i+1][j+1];
-                if(i+1<=n-1 && j-1>=0)
-                down=m[i][j]+dp[i+1][j-1];
-                
-                dp[i][j]=max(left,max(down,diag));
-            }
-        }
+        
+        vector<vector<int>>dp(n,vector<int>(n));
         
         for(int i=0;i<n;i++)
         {
-            ans=max(ans,dp[0][i]);
+            for(int j=0;j<n;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=ma[i][j];
+                }
+                else 
+                {
+                    
+                    int left=INT_MIN,down=INT_MIN,right=INT_MIN;
+                    if(j-1>=0)
+                    left=dp[i-1][j-1];
+                    down=dp[i-1][j];
+                    if(j+1<n)
+                    {
+                        right=dp[i-1][j+1];
+                    }
+                    
+                    dp[i][j]=ma[i][j]+max({left,down,right});
+                }
+            }
         }
-        return ans;
+        return *max_element(dp[n-1].begin(),dp[n-1].end());
     }
 };
 
