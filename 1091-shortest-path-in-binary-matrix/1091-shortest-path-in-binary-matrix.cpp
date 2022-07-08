ef6b325781175f4d24ef901aff40ec87ec8b2846
tejastomarbyte <88239743+tejastomarbyte@users.noreply.github.com>
2022-07-08 20:32:12 +0530

    Time: 121 ms (42.21%), Space: 19.2 MB (84.08%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,10 +3,10 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         queue<pair<int,int>>q;
         int n=grid.size();
-        vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
+        // vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
         if(grid[0][0]==1 or grid[n-1][n-1]==1)
             return -1;
-        vis[0][0]=true;
+        grid[0][0]=1;
         q.push({0,0});
         int dist=1;
         vector<pair<int,int>> d={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
@@ -24,9 +24,9 @@ public:
                 for(int i=0;i<8;i++)
                 {
                     int x1=a+d[i].first,y1=b+d[i].second;
-                    if(x1>=0 && y1>=0 && x1<grid.size() && y1<grid.size() && vis[x1][y1]==false && grid[x1][y1]==0)
+                    if(x1>=0 && y1>=0 && x1<grid.size() && y1<grid.size() && grid[x1][y1]==0)
                     {
-                        vis[x1][y1]=true;
+                        grid[x1][y1]=1;
                         q.push({x1,y1});
                     }
                 }
